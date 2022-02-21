6e68f083336c1ec5e5633f85e162bc454055bcc0
yousuf Muhammad <yousufm4788@hotmail.com>
2022-02-21 15:39:33 +0000

    Properties

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -2,5 +2,11 @@
 {
     public class clsStock
     {
+        public int SofaNumber { get; set; }
+        public int SofaDescription { get; set; }
+        public string SofaColour { get; set; }
+        public object DateAdded { get; set; }
+        public decimal Price { get; set; }
+        public bool Available { get; set; }
     }
 }
