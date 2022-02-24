2fe8dcbc6c9e6b150f222b633ba28bcd856a2611
Ashden Bauer <P2629254@my365.dmu.ac.uk>
2022-02-24 19:46:12 +0000

    created all the tests for class

--- Testing1/tstOrder.cs
+++ Testing1/tstOrder.cs
@@ -15,5 +15,81 @@ namespace Testing1
             //test to see that it exists
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void OrderNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            int TestData = 1;
+            //assign thee data to property
+            AnOrder.OrderNo = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AnOrder.OrderNo, TestData);
+
+        }
+
+        [TestMethod]
+        public void OrderPassPropertyOK()
+        {
+            //create an instance of the class
+            clsOrder AnOrder = new clsOrder();
+            //create some test data
+            Boolean TestData = true;
+            //assign to the property
+            AnOrder.OrderPass = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.OrderPass, TestData);
+        }
+
+        [TestMethod]
+
+        public void EstDeliveryDatePropertyOK()
+        {
+            //create an instance of the class
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign
+            DateTime TestData = DateTime.Now.Date;
+            //asign to the property
+            AnOrder.EstDeliveryDate = TestData;
+            //test to see that the two vals are same
+            Assert.AreEqual(AnOrder.EstDeliveryDate, TestData);
+
+        }
+
+        [TestMethod]
+        public void DeliveryAddressPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+
+            string TestData = "LE1 4AB Leicester";
+
+            AnOrder.DeliveryAddress = TestData;
+
+            Assert.AreEqual(AnOrder.DeliveryAddress, TestData);
+        }
+
+        [TestMethod]
+        public void AutomatedConfEmailPropertyOK()
+        { 
+
+            clsOrder AnOrder = new clsOrder();
+            string TestData = "Order has been confirmed";
+            AnOrder.AutomatedConfEmail = TestData;
+            Assert.AreEqual(AnOrder.AutomatedConfEmail, TestData);
+
+        }
+
+        [TestMethod]
+        public void PaymentDetailsPropertyOK()
+        {
+            clsOrder AnOrder = new clseOrder();
+            Int32 TestData = 1231231;
+            AnOrder.PaymentDetails = TestData;
+            Assert.AreEqual(AnOrder.PaymentDetails, TestData);
+        }
     }
+
+  
 }
