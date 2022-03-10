8580e017185fa60e6e06fdbe23b7756307167e39
yousuf Muhammad <yousufm4788@hotmail.com>
2022-03-10 21:53:46 +0000

    Simple presentation

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -4,11 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+       
+    }
 
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsCustomer
+        clsStock AnStock = new clsStock();
+        //store the address in this session object
+        Session["AnStock"] = AnStock;
+
+        //cpature the Sofa Number
+        AnStock.SofaNumber = Convert.ToInt32(txtSofaNumber.Text);
+
+        //capture the Sofa's description
+        AnStock.SofaDescription = Convert.ToInt32(txtSofaDescription.Text);
+
+        //capture the Sofa Colour
+        AnStock.SofaColour = txtSofaColour.Text;
+
+        //capture the Date Added 
+        AnStock.DateAdded = txtDateAdded.Text;
+
+        //capture the Sofa Price
+        AnStock.Price = Convert.ToDecimal(txtPrice.Text);
+
+        //capture the Sofa's Availablity
+        AnStock.Available = Convert.ToBoolean(txtAvailable.Text);
+        //navigate to the viewer page 
+        Response.Redirect("StockDataEntry.aspx");
     }
 }
--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create a new instance of the clsStock
+        clsStock AnStock = new clsStock();
+        //get the data from the session object
+        AnStock = (clsStock)Session["AnStock"];
+        //display the Sofa number for this entry 
+        Response.Write(AnStock.SofaNumber);
+
+        //display the sofa Description for this entry 
+        Response.Write(AnStock.SofaDescription);
+
+        //display the sofa Colour for this entry 
+        Response.Write(AnStock.SofaColour);
+
+        //display the Date Added for this entry 
+        Response.Write(AnStock.DateAdded);
+
+        //dsiplay the Price for this entry
+        Response.Write(AnStock.Price);
+
+        //display the available for this entry 
+        Response.Write(AnStock.Available);
 
     }
 }
