01465ebeac4dba7e640c5d7d817835dec140e1c2
yousuf Muhammad <yousufm4788@hotmail.com>
2022-04-27 00:38:27 +0100

    Added all of the mins,max and stuff

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -112,5 +112,101 @@ namespace ClassLibrary
                 return false;
             }
         }
+
+        public string Valid(string SofaNumber, string SofaDescription, string SofaColour, string DateTime, string Price, string Available)
+        {
+            //create a string variable to store the error
+            String Error = "";
+            //create a tempory variable to store date values 
+            DateTime DateTemp;
+            //if the sofanumber is blank
+            if (SofaNumber.Length == 0) ;
+            {
+                //record the error 
+                Error = Error + "The sofa number may be blank";
+            }
+            //if the sofa Number is greater than 6 character 
+            if(SofaNumber.Length > 6)
+            {
+                //record the error 
+                Error = Error + "The sofa number must be less then 6 character";
+
+            }
+
+            try
+            {
+                //copy the dateAdded value to the DateTime variable 
+                DateTemp = Convert.ToDateTime(DateAdded);
+                if (DateTemp < DateTime.Now.Date)
+                {
+                    //record the error
+                    Error = Error + "The date cannot be in the past:";
+                }
+                //check to see if the date is greater than today's date
+                if (DateTemp > DateTime.Now.Date)
+                {
+                    //record the error
+                    Error = Error + "The date cannot be in the future : ";
+                }
+            }
+            catch
+            {
+                //record the error
+                Error = Error + "The date was not a valid date : ";
+            }
+
+            //is the post code blank
+            if (SofaDescription.Length == 0)
+            {
+                //record the error
+                Error = Error + "The SofaDescription may not be blank : ";
+            }
+            //if the post code is too long
+            if (SofaDescription.Length > 9)
+            {
+                //record the error
+                Error = Error + "The SofaDescription must be less than 9 characters : ";
+            }
+            //is the street blank
+            if (SofaColour.Length == 0)
+            {
+                //record the error
+                Error = Error + "The SofaColour may not be blank : ";
+            }
+            //if the street is too long
+            if (SofaColour.Length > 50)
+            {
+                //record the error
+                Error = Error + "The SofaColour must be less than 50 characters : ";
+            }
+            //is the town blank
+            if (Price.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Price may not be blank : ";
+            }
+            //if the town is too long
+            if (Price.Length > 50)
+            {
+                //record the error
+                Error = Error + "The Price must be less than 50 characters : ";
+            }
+
+            //is the town blank
+            if (Available.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Available may not be blank : ";
+            }
+            //if the town is too long
+            if (Available.Length > 50)
+            {
+                //record the error
+                Error = Error + "The Available must be less than 50 characters : ";
+            }
+
+            //return any error messages
+            return Error;
+        }
     }
 }
